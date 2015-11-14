426ae501708f3e2c7e73a7f92df2b77a5d20b22a
Hermann Wagner <hermann.wagner01@gmx.at>
2015-11-14 09:31:56 +0100

    Modified staging file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,10 +12,13 @@ Kassi::Application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+
+  # Set how to handle deprecation warnings
+  config.active_support.deprecation = :notify
 
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
