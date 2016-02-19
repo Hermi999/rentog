b25b5b56c946c3cd297da0a630cbd78ef217c388
Hermann Wagner <hermann.wagner01@gmx.at>
2016-02-19 20:30:00 +0100

    Disabled mercury controllers

--- lib/mercury/authentication.rb
+++ lib/mercury/authentication.rb
@@ -1,16 +1,17 @@
-module Mercury
-  module Authentication
+# wah: We dont need Mercury at the moment
+# module Mercury
+#   module Authentication
 
-    def can_edit?
-      @current_user = current_person
-      @current_community = ApplicationController.find_community(community_identifiers)
-      @current_user && @current_community && @current_user.has_admin_rights_in?(@current_community)
-    end
+#     def can_edit?
+#       @current_user = current_person
+#       @current_community = ApplicationController.find_community(community_identifiers)
+#       @current_user && @current_community && @current_user.has_admin_rights_in?(@current_community)
+#     end
 
-    def community_identifiers
-      app_domain = URLUtils.strip_port_from_host(APP_CONFIG.domain)
-      ApplicationController.parse_community_identifiers_from_host(request.host, app_domain)
-    end
+#     def community_identifiers
+#       app_domain = URLUtils.strip_port_from_host(APP_CONFIG.domain)
+#       ApplicationController.parse_community_identifiers_from_host(request.host, app_domain)
+#     end
 
-  end
-end
+#   end
+# end
