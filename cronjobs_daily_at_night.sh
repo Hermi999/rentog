2e26685b743a80e134e1879ac77f17d951dce5e5
Hermann Wagner <hermann.wagner01@gmx.at>
2016-04-23 18:48:58 +0200

    Fixed daily cronjob

--- cronjobs_daily_at_night.sh
+++ cronjobs_daily_at_night.sh
@@ -2,4 +2,4 @@
 
 #cd /home/osboxes/rentog
 cd /var/app/current
-bundle exec rake sharetribe:deliver_device_return_notifications:deliver
+bundle exec rake sharetribe:device_return_notifications:deliver
