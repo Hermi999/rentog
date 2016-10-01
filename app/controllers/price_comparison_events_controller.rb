712c3061cf41c4a847d29bd93f29cf082a378abb
hermi999 <hermann.wagner01@gmx.at>
2016-10-01 16:52:29 +0200

    fixed price comparison errors

--- app/controllers/price_comparison_events_controller.rb
+++ app/controllers/price_comparison_events_controller.rb
@@ -72,7 +72,7 @@ class PriceComparisonEventsController < ApplicationController
 			query_rentog = ""
 
 			if title.length > 1
-				query = "(model LIKE '%" + title[1].strip + "%' AND manufacturer LIKE '%" + title[0].strip + "%') OR title LIKE '%" + title[1].strip + "%'"
+				query = "(model LIKE '%" + title[1].strip + "%' AND manufacturer LIKE '%" + title[0].strip + "%') OR (title LIKE '%" + title[1].strip + "%' AND title LIKE '%" + title[0].strip + "%')"
 				query_rentog = "title LIKE '%" + title[1].strip + "%' AND title LIKE '%" + title[0].strip + "%'"
 			else
 				query = "model LIKE '%" + title[0].strip + "%' OR title LIKE '%" + title[0].strip + "%'"
@@ -80,28 +80,32 @@ class PriceComparisonEventsController < ApplicationController
 			end
 
 			# rentog database
-			rentog_result = Listing.where(query_rentog).map do |x|
-				cf_condition_id = CustomFieldName.where(value: "condition").first.custom_field_id
+			rentog_result = Listing.where(query_rentog + " AND deleted = false AND open = true").map do |x|
+				type = x.get_listing_type
+				if type == "sell" || type == "rent" || type == "ad"
+					
+					cf_condition_id = Maybe(CustomFieldName.where(value: "condition").first).custom_field_id.or_else(nil)
 
-				cfv_condition_id = CustomFieldValue.where(custom_field_id: cf_condition_id, listing_id: x.id).first.id
-				condition = CustomFieldOptionSelection.where(custom_field_value_id: cfv_condition_id).first.custom_field_option.title
+					cfv_condition_id = Maybe(CustomFieldValue.where(custom_field_id: cf_condition_id, listing_id: x.id).first).id.or_else(nil)
+					condition = Maybe(CustomFieldOptionSelection.where(custom_field_value_id: cfv_condition_id).first).custom_field_option.title.or_else(nil)
 
-				price = x.price_cents ? (x.price_cents / 100).to_s : "On request"
-				currency = (price == "On request") ? "" : x.currency
-				country = Maybe(ISO3166::Country.find_country_by_name(Maybe(x.author.location.address).split(",").last.gsub(/[^a-zÖÜÄüöä\s]/i, '').strip.or_else(nil))).translation(I18n.locale).or_else("")
+					price = x.price_cents ? (x.price_cents / 100).to_s : "On request"
+					currency = (price == "On request") ? "" : x.currency
+					country = Maybe(ISO3166::Country.find_country_by_name(Maybe(x.author.location.address).split(",").last.gsub(/[^a-zÖÜÄüöä\s]/i, '').strip.or_else(nil))).translation(I18n.locale).or_else("")
 
-				{
-					model: x.title.split(" (")[0],
-					manufacturer: x.title.split(" (")[1].sub!(")", ""),
-					price: price,
-					currency: x.currency.to_s,
-					country: country,
-					currency: currency,
-					renting_price_period: x.unit_type.to_s,
-					seller: "RENTOG",
-					condition: condition,
-					link: listing_url(x)	
-				}
+					{
+						model: x.title.split(" (")[0],
+						manufacturer: x.title.split(" (")[1].sub!(")", ""),
+						price: price,
+						currency: x.currency.to_s,
+						country: country,
+						currency: currency,
+						renting_price_period: x.unit_type.to_s,
+						seller: "RENTOG",
+						condition: condition,
+						link: listing_url(x)	
+					}
+				end
 			end
 
 			# price comparison database
