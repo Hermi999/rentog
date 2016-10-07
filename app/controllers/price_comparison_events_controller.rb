10f2ef8611d66a3cda3a87a4513a8efed749b4e0
hermi999 <hermann.wagner01@gmx.at>
2016-10-07 08:31:30 +0200

    enabled multi-device search

--- app/controllers/price_comparison_events_controller.rb
+++ app/controllers/price_comparison_events_controller.rb
@@ -8,6 +8,14 @@ class PriceComparisonEventsController < ApplicationController
 			render :json => {status: "error", message: "limit reached"} and return
 		end
 
+		# extract result
+		titles = params[:price_comparison_params][:device_name].map do |x|
+			x.split("|")
+		end
+		titles.delete([])
+
+		params[:price_comparison_params][:device_name] = params[:price_comparison_params][:device_name].to_s
+
 		ev = PriceComparisonEvent.new(price_comparison_params)
 		ev.update_attribute(:ipAddress, request.ip)
 
@@ -28,7 +36,7 @@ class PriceComparisonEventsController < ApplicationController
 				Delayed::Job.enqueue(PriceComparisonJob.new(ev.id, @current_community.id))
 
 			elsif params["price_comparison_params"]["action_type"] == "device_chosen"
-				result = extract_result_from_db
+				result = extract_result_from_db(titles)
 
 			elsif params["price_comparison_params"]["action_type"] == "lead_generated"
 
@@ -64,82 +72,83 @@ class PriceComparisonEventsController < ApplicationController
 			end
 		end
 
-		def extract_result_from_db
-			# extract result
-			title = params[:price_comparison_params][:device_name].split("|")
-			query_price_comp = nil
-			query_rentog = nil
-			q_1 = "price_cents <> '' AND "
-			q_2 = "deleted = false AND open = true AND price_cents <> '' AND "
+		def extract_result_from_db(titles)
+			ret = titles.map do |title|
+				query_price_comp = nil
+				query_rentog = nil
+				q_1 = "price_cents <> '' AND "
+				q_2 = "deleted = false AND open = true AND price_cents <> '' AND "
 
-			if title.length > 1 
-				query_price_comp = PriceComparisonDevice.where(q_1 + "((model LIKE ? AND manufacturer LIKE ?) OR (title LIKE ? AND title LIKE ?))", "%" + title[1].strip + "%", "%" + title[0].strip + "%", "%" + title[1].strip + "%", "%" + title[0].strip + "%")
-				query_rentog = Listing.where(q_2 + "(title LIKE ? AND title LIKE ?)", "%" + title[1].strip + "%", "%" + title[0].strip + "%")
-			else
-				query_price_comp = PriceComparisonDevice.where(q_1 + "(model LIKE ? OR title LIKE ?)", "%" + title[0].strip + "%", "%" + title[0].strip + "%")
-				query_rentog = Listing.where(q_2 + "(title LIKE ?)", "%" + title[0].strip + "%")
-			end
+				if title.length > 1 
+					query_price_comp = PriceComparisonDevice.where(q_1 + "((model LIKE ? AND manufacturer LIKE ?) OR (title LIKE ? AND title LIKE ?))", "%" + title[1].strip + "%", "%" + title[0].strip + "%", "%" + title[1].strip + "%", "%" + title[0].strip + "%")
+					query_rentog = Listing.where(q_2 + "(title LIKE ? AND title LIKE ?)", "%" + title[1].strip + "%", "%" + title[0].strip + "%")
+				else
+					query_price_comp = PriceComparisonDevice.where(q_1 + "(model LIKE ? OR title LIKE ?)", "%" + title[0].strip + "%", "%" + title[0].strip + "%")
+					query_rentog = Listing.where(q_2 + "(title LIKE ?)", "%" + title[0].strip + "%")
+				end
 
-			# rentog database
-			rentog_result = query_rentog.map do |x|
-				price = x.price_cents ? (x.price_cents / 100).to_s : "On request"
+				# rentog database
+				rentog_result = query_rentog.map do |x|
+					price = x.price_cents ? (x.price_cents / 100).to_s : "On request"
 
-				type = x.get_listing_type
-				if type == "sell" || type == "rent" || type == "ad"
-					
-					cf_condition_id = Maybe(CustomFieldName.where(value: "condition").first).custom_field_id.or_else(nil)
+					type = x.get_listing_type
+					if type == "sell" || type == "rent" || type == "ad"
+						
+						cf_condition_id = Maybe(CustomFieldName.where(value: "condition").first).custom_field_id.or_else(nil)
 
-					cfv_condition_id = Maybe(CustomFieldValue.where(custom_field_id: cf_condition_id, listing_id: x.id).first).id.or_else(nil)
-					condition = Maybe(CustomFieldOptionSelection.where(custom_field_value_id: cfv_condition_id).first).custom_field_option.title.or_else(nil)
+						cfv_condition_id = Maybe(CustomFieldValue.where(custom_field_id: cf_condition_id, listing_id: x.id).first).id.or_else(nil)
+						condition = Maybe(CustomFieldOptionSelection.where(custom_field_value_id: cfv_condition_id).first).custom_field_option.title.or_else(nil)
 
+						currency = (price == "On request") ? "" : x.currency
+						country = Maybe(ISO3166::Country.find_country_by_name(Maybe(x.author.location.address).split(",").last.gsub(/[^a-zÖÜÄüöä\s]/i, '').strip.or_else(nil))).translation(I18n.locale).or_else("") if x.author.location
+
+						{
+							model: x.title.split(" (")[0],
+							manufacturer: Maybe(x.title.split(" (")[1]).sub!(")", "").or_else(nil),
+							price: price,
+							currency: x.currency.to_s,
+							country: country,
+							currency: currency,
+							renting_price_period: x.unit_type.to_s,
+							seller: "RENTOG",
+							condition: condition,
+							link: listing_url(x)	
+						}
+					end
+				end
+
+				# price comparison database
+				result = query_price_comp.map do |x| 
+					price = x.price_cents ? (x.price_cents / 100).to_s : "On request"
+					link = x.seller_contact ? x.seller_contact : x.device_url
+					if !link.empty?
+						link.sub!(" ", "")
+						link = "http://" + link if link.starts_with? "www."
+						link = link.split("?").first + "?referrer=rentog_price_comparison_tool"
+					else
+						link = "#"
+					end
 					currency = (price == "On request") ? "" : x.currency
-					country = Maybe(ISO3166::Country.find_country_by_name(Maybe(x.author.location.address).split(",").last.gsub(/[^a-zÖÜÄüöä\s]/i, '').strip.or_else(nil))).translation(I18n.locale).or_else("") if x.author.location
+					model = x.model.to_s.empty? ? x.title.to_s : x.model.to_s
 
 					{
-						model: x.title.split(" (")[0],
-						manufacturer: Maybe(x.title.split(" (")[1]).sub!(")", "").or_else(nil),
+						model: model,
+						manufacturer: x.manufacturer.to_s,
 						price: price,
 						currency: x.currency.to_s,
-						country: country,
+						country: x.seller_country.to_s,
 						currency: currency,
-						renting_price_period: x.unit_type.to_s,
-						seller: "RENTOG",
-						condition: condition,
-						link: listing_url(x)	
+						renting_price_period: x.renting_price_period.to_s,
+						seller: x.seller.to_s,
+						dev_type: x.dev_type.to_s,
+						condition: x.condition.to_s,
+						link: link
 					}
 				end
+				Maybe(rentog_result).or_else([]) + Maybe(result).or_else([])
 			end
 
-			# price comparison database
-			result = query_price_comp.map do |x| 
-				price = x.price_cents ? (x.price_cents / 100).to_s : "On request"
-				link = x.seller_contact ? x.seller_contact : x.device_url
-				if !link.empty?
-					link.sub!(" ", "")
-					link = "http://" + link if link.starts_with? "www."
-					link = link.split("?").first + "?referrer=rentog_price_comparison_tool"
-				else
-					link = "#"
-				end
-				currency = (price == "On request") ? "" : x.currency
-				model = x.model.to_s.empty? ? x.title.to_s : x.model.to_s
-
-				{
-					model: model,
-					manufacturer: x.manufacturer.to_s,
-					price: price,
-					currency: x.currency.to_s,
-					country: x.seller_country.to_s,
-					currency: currency,
-					renting_price_period: x.renting_price_period.to_s,
-					seller: x.seller.to_s,
-					dev_type: x.dev_type.to_s,
-					condition: x.condition.to_s,
-					link: link
-				}
-			end
-
-			return rentog_result + result
+			ret
 		end
 
 
